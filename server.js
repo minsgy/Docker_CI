cde7d670db7b42a6d5ebc82022194267873cb685
minsgy <alstjrchl97@naver.com>
2021-02-02 01:59:32 +0900

    server Application

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require("express");
-const PORT = 8080;
+const express = require('express');
+const redis = require('redis');
 
 const app = express();
-app.get('/', (req,res)=>{
-    res.send('asdfasdf');
+
+
+const client = redis.createClient({
+    // host는 도커 환경이 아닐 때, 바로 URL로 주지만,
+    // 도커의 경우는 docker-compose.yml 파일에 명시한 컨테이너를 줘야됌
+    host: "redis-server",
+    port: "6379"
 })
 
-app.listen(PORT);
-console.log("SERVER ON")
+client.set("number", 0);
+app.get('/', (req, res) => {
+    client("number", (err, number) => {
+        // 현재 숫자를 가져 온 후, 1씩 올려줍니다.
+        client.set("number", parseInt(number) + 1)
+        res.send("숫자가 1씩 증가 ㅋ: " + number)
+    })
+})
+
+
+app.listen(8080);
+console.log('server is running');
